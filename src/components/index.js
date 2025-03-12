19e6c694a9b7ea11cad03e1e6dc8c8bd7396bd60
Ajay Rathore <ajayxrathore@gmail.com>
2025-03-12 20:36:30 +0530

    add Login , Select , Signup and AuthLayout components

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,25 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-import Container from "../components/Container/Container"
+import Container from "../components/Container/Container";
+import Input from "./Input";
+import Button from "./Button";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
+import AuthLayout from "./AuthLayout";
 export {
-    Header,
-    Footer,
-    Logo,
-    LogoutBtn,
-    Container
-}
+  Header,
+  Footer,
+  Logo,
+  LogoutBtn,
+  Container,
+  Button,
+  Input,
+  Select,
+  PostCard,
+  Login,
+  Signup,
+  AuthLayout,
+};
